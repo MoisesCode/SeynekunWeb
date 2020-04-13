6fd88922f2ce0f915485ffb34d86b3eaf1943fe0
MoisesCode <mvilladiegos@unicesar.edu.co>
2020-04-13 00:14:36 -0500

    first commit

---
+++ Datos/ConnectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class ConnectionManager
+    {
+        internal SqlConnection conexion;
+        public ConnectionManager(string connectionString)
+        {
+            conexion = new SqlConnection(connectionString);
+        }
+        public void Open()
+        {
+            conexion.Open();
+        }
+        public void Close()
+        {
+            conexion.Close();
+        }
+    }
+}
